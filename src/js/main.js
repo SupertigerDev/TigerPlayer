61a18c0a37b129840e8d4b476236694fbabf494d
supertiger1234 <saawalkhn@googlemail.com>
2018-07-03 21:08:45 +0100

    More files checking.

--- src/js/main.js
+++ src/js/main.js
@@ -83,37 +83,44 @@ $(function () {
     function getAllMusic() {
         var filesFolders = []
         fs.readdir(store.get("configuration").musicFolderPath[0], function (err, items) {
-            if (typeof items == "undefined") {
-                // event.sender.send('filesSend', false)
-                //return
-            }
             for (var i = 0; i < items.length; i++) {
-                filesFolders.push({
-                    name: items[i],
-                    isDirectory: fs.lstatSync(store.get("configuration").musicFolderPath[0] + "/" + items[i]).isDirectory()
-                })
+                if (items[i].endsWith(".mp3")) {
+                    filesFolders.push({
+                        name: items[i],
+                        isDirectory: fs.lstatSync(store.get("configuration").musicFolderPath[0] + "/" + items[i]).isDirectory()
+                    })
 
-                var tags = {};
-                jsmediatags.read(store.get("configuration").musicFolderPath[0] + "/" + items[i], {
-                    onSuccess: function (tag) {
-                        var picture = tag.tags.picture; // create reference to track art
-                        var base64String = "";
-                        for (var i = 0; i < picture.data.length; i++) {
-                            base64String += String.fromCharCode(picture.data[i]);
+                    var tags = {};
+                    jsmediatags.read(store.get("configuration").musicFolderPath[0] + "/" + items[i], {
+                        onSuccess: function (tag) {
+                            var picture = tag.tags.picture; // create reference to track art
+                            var base64String = "";
+                            for (var i = 0; i < picture.data.length; i++) {
+                                base64String += String.fromCharCode(picture.data[i]);
+                            }
+                            var imageUri = "data:" + picture.format + ";base64," + window.btoa(base64String);
+
+                            var item = $('<div class="musicAppend"><img class="musicImage" src="' + imageUri + '"></img><div class="information"><p>' + tag.tags.title + '</p><p>' + tag.tags.artist + '</p><p>0:00</p></div></div>').hide();
+                            $(musicList).append(item);
+                            item.fadeIn(300);
+                        },
+                        onError: function (error) {
+                            // handle error
+                            console.log(error);
                         }
-                        var imageUri = "data:" + picture.format + ";base64," + window.btoa(base64String);
-                        musicList.append('<div class="musicAppend"><img class="musicImage" src="' + imageUri + '"></img><div class="information"><p>' + tag.tags.title + '</p><p>' + tag.tags.artist + '</p><p>0:00</p></div></div>')
-                        console.log(tag)
-                    },
-                    onError: function (error) {
-                        // handle error
-                        console.log(error);
-                    }
-                });
-
+                    });
+                }
 
             }
-
+            if (filesFolders.length <= 0) {
+                firstStartScreen.css("display", "block")
+                mainContainer.animate({
+                    opacity: 0
+                }, 300)
+                firstStartScreen.animate({
+                    opacity: 1
+                }, 300)
+            }
 
 
 
